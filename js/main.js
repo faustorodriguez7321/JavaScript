3f4535191f0ac62f9e04b6f61b0c5d99794bf125
Fausto Rodriguez S. <faustorodriguez7321@gmail.com>
2026-08-25 18:25:48 -0300

    primer entrega

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//comentario en linea
+
+
+/*comentario en bloque
+*/
+const nombre=prompt("Ingrese su nombre");
+const apellido=prompt("Ingrese su apellido");
+const anionaci=prompt("Ingrese su año de nacimiento");
+
+let edad = 2026 - anionaci;
+
+alert(nombre + " " + apellido);
+alert(edad)
