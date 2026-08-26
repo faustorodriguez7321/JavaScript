23c174eed261e59f71ff4f24a7d5f64c4f1cd7f3
Fausto Rodriguez S. <faustorodriguez7321@gmail.com>
2026-08-26 11:41:03 -0300

    primer entrega

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 //comentario en linea
+
+
 /*comentario en bloque
 */
-/* el condicional OR se escribe || el AND se escribe && */
-for (let i = 0;i < 2;i++) {
-    let nombre = prompt("Ingrese su nombre");
-    let apellido = prompt("Ingrese su apellido");
-    let anionaci = prompt("Ingrese su año de nacimiento");
-    let edad = 2026 - anionaci;
+const nombre=prompt("Ingrese su nombre");
+const apellido=prompt("Ingrese su apellido");
+const anionaci=prompt("Ingrese su año de nacimiento");
 
+let edad = 2026 - anionaci;
 
-    if (edad >= 18) {
-        alert("usted es mayor de edad " + edad + " " + nombre + " " + apellido)
-    }
-    else {
-        alert("usted es menor de edad " + edad + " " + nombre + " " + apellido)
-    }
-}
+alert(nombre + " " + apellido);
+alert(edad)
